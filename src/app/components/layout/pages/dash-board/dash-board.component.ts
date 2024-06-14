9b19fd4b0da87570d9b2681017b515e85a64cf10
Pablo Andres Chacon <pabloandres120297@gmail.com>
2024-06-14 17:30:17 -0500

    añadiendo el dashboard

--- src/app/components/layout/pages/dash-board/dash-board.component.ts
+++ src/app/components/layout/pages/dash-board/dash-board.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Chart,registerables } from 'chart.js'
+import { DashboardService } from '../../../../services/dashboard.service';
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
   styleUrl: './dash-board.component.css'
 })
-export class DashBoardComponent {
+export class DashBoardComponent implements OnInit{
+  
+  totalIngresos: string = "0"
+  totalVentas:string = "0"
+  totalProductos:string = "0"
 
+  constructor(
+    private _dashboardServicio: DashboardService
+  ) {}
+
+  mostarGrafico(labelGrafico: any[], dataGrafico: any) {
+    const charBarras = new Chart('charBarras', {
+      type: 'bar',
+      data: {
+        labels: labelGrafico,
+        datasets: [{
+          label: '# de ventas',
+          data: dataGrafico,
+          backgroundColor: [
+            'rgba(54,162,235,0.2)'
+          ],
+          borderColor: [
+            'rgba(54,162,235,1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this._dashboardServicio.resumen().subscribe({
+      next: (data) => {
+        if (data.status) {
+          this.totalIngresos = data.value.totalIngresos
+          this.totalVentas = data.value.totalVentas
+          this.totalProductos = data.value.totalProductos
+
+          const arrayData: any[] = data.value.ventasUltimaSemana
+          console.log(arrayData)
+
+          const labelTemp = arrayData.map((value) => value.fecha)
+          const dataTemp = arrayData.map((value) => value.total)
+
+          this.mostarGrafico(labelTemp, dataTemp)
+        }
+      },
+      error: (e) => {}
+    })
+  }
 }
